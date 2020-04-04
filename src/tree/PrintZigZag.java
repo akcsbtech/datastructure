bb3c23fecead10e5d0bd49c432cb13d1845a2466
akashmits <akashgoyal311@gmail.com>
2020-04-04 17:13:01 +0530

    latest code

--- src/tree/PrintZigZag.java
+++ src/tree/PrintZigZag.java
@@ -1,8 +1,7 @@
 package tree;
 
-import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 import dpproblem.TreeNode;
 
@@ -92,6 +91,59 @@ public class PrintZigZag {
 				leftToRight = true;
 			}
 
+		}
+	}
+
+	void printZigZagTraversal(Node root) {
+
+		// if null then return
+		if (root == null) {
+			return;
+		}
+
+		// declare two stacks
+		Stack<Node> currentLevel = new Stack<>();
+		Stack<Node> nextLevel = new Stack<>();
+
+		// push the root
+		currentLevel.push(root);
+		boolean leftToRight = true;
+
+		// check if stack is empty
+		while (!currentLevel.isEmpty()) {
+
+			// pop out of stack
+			Node node = currentLevel.pop();
+
+			// print the data in it
+			System.out.print(node.data + " ");
+
+			// store data according to current
+			// order.
+			if (leftToRight) {
+				if (node.left != null) {
+					nextLevel.push(node.left);
+				}
+
+				if (node.right != null) {
+					nextLevel.push(node.right);
+				}
+			} else {
+				if (node.right != null) {
+					nextLevel.push(node.right);
+				}
+
+				if (node.left != null) {
+					nextLevel.push(node.left);
+				}
+			}
+
+			if (currentLevel.isEmpty()) {
+				leftToRight = !leftToRight;
+				Stack<Node> temp = currentLevel;
+				currentLevel = nextLevel;
+				nextLevel = temp;
+			}
 		}
 	}
 }
